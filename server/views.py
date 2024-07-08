613106d5dfe7aac1de8dc864c6dd76feffda17ed
Alejandro Ponce <122680586+alejandroponce00@users.noreply.github.com>
2024-07-08 19:24:30 -0300

    login y registro funcionando

--- server/views.py
+++ server/views.py
@@ -4,14 +4,20 @@ from .serializers import UserSerializer
 from rest_framework.authtoken.models import Token   
 from django.contrib.auth.models import User
 from rest_framework import status
+from django.shortcuts import get_object_or_404
 
 
 @api_view(['POST'])
 def login(request):
+    user = get_object_or_404(User, username=request.data['username'])
     
+    if not user.check_password(request.data['password']):
+        return Response({"error": "invalid password"}, status=status.HTTP_400_BAD_REQUEST)
     
-    
-    return Response({})
+    token, created = Token.objects.get_or_create(user=user) 
+    serializer = UserSerializer(instance=user)    
+   
+    return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
 
 @api_view(['POST'])
 def register(request):
@@ -32,5 +38,5 @@ def register(request):
  
  
 @api_view(['POST'])
-def profile(request):
+def profile(request): 
     return Response({})
